4d699e5f5ccbebe653db7a87037ea3a304091c44
serenaliguori <serenaliguori28@gmail.com>
2021-05-24 20:05:53 +0200

    Aggiunta servletLogout

--- Progetto-HomeGym/src/it/unisa/controller/servletLogout.java
+++ Progetto-HomeGym/src/it/unisa/controller/servletLogout.java
@@ -6,12 +6,16 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/servletLogout")
 public class servletLogout extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		session.invalidate();
+		response.sendRedirect("index.jsp");
 	
 	}
 
